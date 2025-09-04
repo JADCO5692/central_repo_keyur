a7d6fc2cc5dccc543c9328394325b30937fa4e5d
keyur <keyur.techultra@gmail.com>
2025-09-04 09:30:19 +0530

    [IMP] adding dropship report changes

--- v17/chris_m/custom_dropstitch/models/stock_move.py
+++ v17/chris_m/custom_dropstitch/models/stock_move.py
@@ -119,5 +119,5 @@ class StockMoveLine(models.Model):
                 aggregated_move_lines[aggregated_move_line]['customer_sku'] = ', '.join(customer_products)
             move = aggregated_move_lines[aggregated_move_line]['move']
             if move and move.custom_item_image:
-                aggregated_move_lines[aggregated_move_line]['item_image'] = base64.b64encode(move.custom_item_image).decode('utf-8')
+                aggregated_move_lines[aggregated_move_line]['item_image'] = base64.b64encode(move.custom_item_image)
         return aggregated_move_lines
