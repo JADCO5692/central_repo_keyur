58ba4d47ee95ef8706e427f16b7f5f71cd39d68e
keyur <keyur.techultra@gmail.com>
2025-09-06 19:51:23 +0530

    [IMP] margin on pricelist

--- v18/shahir/custom_boxesofhouston/models/product_pricelist_item.py
+++ v18/shahir/custom_boxesofhouston/models/product_pricelist_item.py
@@ -11,8 +11,30 @@ class ProductPricelistItem(models.Model):
     price_margin = fields.Float(
         string="Price Margin",
         digits="Product Price",
+        compute="compute_price_margin",
         help="The margin to apply to the cost price, expressed as a percentage.",
     )
+    product_cost = fields.Float(
+        string="Cost Price",
+        related="product_tmpl_id.standard_price",
+        digits="Product Price",
+        store=True,
+        readonly=True,
+    )
+    margin_percent = fields.Float(
+        string="Margin Percent",
+        digits="Product Price",
+        compute="compute_price_margin")
+
+    @api.depends('product_tmpl_id', 'compute_price','fixed_price')
+    def compute_price_margin(self):
+        for item in self:
+            item.price_margin = 0.0
+            item.margin_percent = 0.0
+            if item.display_applied_on == "1_product" and item.product_tmpl_id and item.compute_price == 'fixed':
+                item.price_margin = item.fixed_price - (item.product_tmpl_id.standard_price * 1)
+                if item.price_margin:
+                    item.margin_percent = item.fixed_price and (item.price_margin/item.fixed_price) or 0.0
 
     def _compute_price(self, product, quantity, uom, date, currency=None):
         if self.base == "cost_margin":
